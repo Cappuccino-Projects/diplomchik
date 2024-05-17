1431a59fa71c7f7320ba67904982941ba8cd8bd1
Sergey <markrut23@mail.ru>
2024-05-17 22:07:46 +0400

    Конец

--- client/src/Components/Modal/index.jsx
+++ client/src/Components/Modal/index.jsx
@@ -1,11 +1,14 @@
 import { closeModal } from '@redux/slices/modalsSlice'
 import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import ReactDOM from 'react-dom'
 
 import { BuyConfirm } from '@components/Modal/BuyConfirm'
 import { GetRewards } from '@components/Modal/GetRewards'
 import { ModalWindow } from '@components/Modal/ModalWindow'
 import { RewardsNotAvalible } from '@components/Modal/RewardsNotAvalible'
+
+const portal = document.getElementById('portal')
 
 export const Modal = () => {
 	const dispatch = useDispatch()
@@ -15,13 +18,14 @@ export const Modal = () => {
 		dispatch(closeModal())
 	}, [])
 
-	return (
+    return ReactDOM.createPortal(
 		<ModalWindow isOpen={isOpen} close={closeCallback}>
-			{modalType === 'BUYCONFIRM' && <BuyConfirm close={closeCallback}/>}
+			{modalType === 'BUYCONFIRM' && <BuyConfirm close={closeCallback} />}
 			{modalType === 'REWARDSNOTAVALIBLE' && (
 				<RewardsNotAvalible close={closeCallback} />
 			)}
 			{modalType === 'GETREWARDS' && <GetRewards close={closeCallback} />}
-		</ModalWindow>
+		</ModalWindow>,
+		portal
 	)
 }
