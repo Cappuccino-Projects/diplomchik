6c7db44dcdb4e899945ffd08099f572f9be14f6f
makcext <makcbowka@gmail.com>
2024-05-20 01:27:22 +0300

    allplaces fix & places list render

--- client/src/pages/AllPlaces/index.jsx
+++ client/src/pages/AllPlaces/index.jsx
@@ -12,6 +12,10 @@ export const AllPlaces = () => {
 	const city = useSelector((state) => state.cities)
 	const places = useSelector((state) => state.places)
 
+	console.log(places)
+
+
+	
 	return (
 		<div className={styles.MenuWrapper}>
 			AllPlaces
@@ -20,21 +24,27 @@ export const AllPlaces = () => {
 				<MinimizeMenuButton />
 			</div>
 			<BackToMainMenu />
-			<PlacesWrapper
-				places={places.filter((CurrentPlace) => CurrentPlace.PlaceId < 4)}
-				WrapperText="Недавнее"
-				WrapperButtonEnabled={false}
-			/>
-			<PlacesWrapper
-				places={places.filter((CurrentPlace) => CurrentPlace.PlaceId < 9)}
-				WrapperText="Рекомендуемое"
-				WrapperButtonEnabled={false}
-			/>
-			<PlacesWrapper
-				places={places}
-				WrapperText={places.length + ' мест найдено'}
-				WrapperButtonEnabled={false}
-			/>
+
+
+			
+<PlacesWrapper
+    places={Array.isArray(places) ? places.filter((CurrentPlace) => CurrentPlace && CurrentPlace.PlaceId < 4) : []}
+    WrapperText="Недавнее"
+    WrapperButtonEnabled={false}
+/>
+
+<PlacesWrapper
+    places={Array.isArray(places) ? places.filter((CurrentPlace) => CurrentPlace.PlaceId < 9) : []}
+    WrapperText="Рекомендуемое"
+    WrapperButtonEnabled={false}
+/>
+
+<PlacesWrapper
+    places={Array.isArray(places.places) ? places.places : []}
+    WrapperText={(Array.isArray(places.places) ? places.places.length : 0) + ' мест найдено'}
+    WrapperButtonEnabled={false}
+/>
+
 			<UserCard ShowLvl={false} ShowBalance={false} />
 		</div>
 	)
