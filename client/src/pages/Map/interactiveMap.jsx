f8f086ad80ba2ad1280182e2d08f8e948b7702f6
makcext <makcbowka@gmail.com>
2024-05-20 22:03:18 +0300

    :wp markers from store

--- client/src/pages/Map/interactiveMap.jsx
+++ client/src/pages/Map/interactiveMap.jsx
@@ -1,19 +1,24 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import { useSelector } from 'react-redux'
 
 const InteractiveMap = () => {
-const position = [55.751244, 37.618423] // Moscow's coordinates [lat, lng]
+  const position = [55.751244, 37.618423] // Moscow's coordinates [lat, lng]
+  const places = useSelector((state) => state.places) || [] // Access places from the store and default to an empty array if it's not an array
+
   return (
-    <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={position} zoom={5} style={{ height: "100vh", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {Array.isArray(places.places) && places.places.map((place, index) => (
+        <Marker key={index} position={[place.latitude, place.longitude]}>
+          <Popup>
+            {place.name}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   )
 }
