402ce28758e948fc48b2e4e5d6566d60823144cd
makcext <makcbowka@gmail.com>
2024-05-21 16:31:57 +0300

    edit selected marker state

--- client/src/pages/MapEditMenu/index.jsx
+++ client/src/pages/MapEditMenu/index.jsx
@@ -1,44 +1,15 @@
 import { TitleWrapper, MinimizeMenuButton, BackToMainMenu, UserCard } from '@components'
-import  EditMarker  from '@components/interactiveMap/EditMarker'
+import EditMarker from '@components/interactiveMap/EditMarker'
 import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
 
 import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { openGetRewards, openRewardsNotAvalible, openAddPlaceToMap } from '@redux/slices/modalsSlice'
-import { updateMarker } from '@app/redux/slices/locationsSlice'
+// import { useDispatch, useSelector } from 'react-redux'
+
 
 export const MapEditMenu = (props) => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-	const [showEditMarker, setShowEditMarker] = useState(false);
-  const dispatch = useDispatch()
+	const [showEditMarker, setShowEditMarker] = useState(false)
 
-  const openModal = (isRewardsAvailable) => {
-    setIsModalOpen(true)
-    if (isRewardsAvailable) {
-      dispatch(openAddPlaceToMap())
-    } else {
-      dispatch(openAddPlaceToMap())
-    }
-  }
-
-  const selectedMarker = useSelector((state) => (state.markers ? state.markers.selected : undefined))
-  const markers = useSelector((state) => (state.markers ? state.markers.markers : undefined))
-  const [marker, setMarker] = useState(selectedMarker)
-
-  const handleEdit = (event) => {
-    event.preventDefault();
-
-    const updatedMarker = {
-      ...selectedMarker,
-      [event.target.name]: event.target.value, // Update the property of the selected marker that corresponds to the name of the input field
-    };
-
-    dispatch(updateSelectedMarker(updatedMarker)); // Dispatch the updateSelectedMarker action with the updated marker as the payload
-  };
-
-	console.log('selectedMarker', selectedMarker)
-	console.log('markers', markers)
 
 	return (
 		<div className={styles.MenuWrapper}>
@@ -49,30 +20,20 @@ export const MapEditMenu = (props) => {
 			<BackToMainMenu />
 			<p className={styles.TitleText}>Редактирование карты</p>
 			<div style={{ marginBottom: 'auto' }} className={styles.MainMenuButtonsWrapper}>
-				<div className={styles.MenuButton} onClick={openModal}>
+				<div className={styles.MenuButton}>
 					<i className="fi fi-sr-add" />
 					<p>Добавить объект</p>
 				</div>
-				
-				<EditMarker />
-				
-				<div>
+
 
 
 				<div>
-      <button onClick={() => setShowEditMarker(true)}>Edit Marker</button>
-      {showEditMarker && <EditMarker />}
-    </div>
-
-
+						<div className={styles.MenuButton} onClick={() => setShowEditMarker(!showEditMarker)}>
+							<i className="fi fi-sr-edit" />
+							<p>{showEditMarker ? 'Редактировать объект' : 'Редактировать объект'}</p>
+						</div>
+					{showEditMarker && <EditMarker />} 
 				</div>
-
-				<Link to="/favourite">
-					<div className={styles.MenuButton}>
-						<i className="fi fi-sr-edit" />
-						<p>Редактировать объект</p>
-					</div>
-				</Link>
 
 				<Link to="/favourite">
 					<div className={styles.MenuButton}>
