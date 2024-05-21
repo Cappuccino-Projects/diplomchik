7c16fde7df5a6ec3fb43410a0e70c7e4becd5fd4
makcext <makcbowka@gmail.com>
2024-05-21 09:58:16 +0300

    wp

--- client/src/pages/Map/interactiveMap.jsx
+++ client/src/pages/Map/interactiveMap.jsx
@@ -2,12 +2,12 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { useSelector } from 'react-redux'
 
-const InteractiveMap = () => {
-  const position = [55.751244, 37.618423] // Moscow's coordinates [lat, lng]
+const InteractiveMap = (props) => {
+  const position = props.position || [55.751244, 37.618423] // Use props.position if provided, else default to Moscow's coordinates
   const places = useSelector((state) => state.places) || [] // Access places from the store and default to an empty array if it's not an array
 
   return (
-    <MapContainer center={position} zoom={5} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={position} zoom={props.zoom || 5} style={{ height: "100vh", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
