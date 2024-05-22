ad0242278bcd825b74a74dd3f7f2969da9dd02cc
Ovol <ovol@d3s.ru>
2024-05-22 20:39:13 +0400

    Update user and changes api

---
+++ api/DiplomApi.Models/DTOs/Change/ChangeUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiplomApi.Models.DTOs.Change;
+
+public class ChangeUpdateDto : ChangeCreationDto
+{
+}
---
+++ api/DiplomApi.Models/DTOs/ChangeTypeCreationDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiplomApi.Models.DTOs;
+
+public class ChangeTypeCreationDto
+{
+    public string Name { get; set; } = null!;
+}
