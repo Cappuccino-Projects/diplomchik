3720eb04edef3cef61419458b10b6b7163281897
Sergey <markrut23@mail.ru>
2024-05-23 04:10:46 +0400

    там сям

--- client/src/Components/UserCard/index.jsx
+++ client/src/Components/UserCard/index.jsx
@@ -3,7 +3,8 @@ import styles from './styles.module.css'
 import { ProgressBar } from '@components'
 import { useGetUserByIdQuery } from '@app/redux/services/userApi'
 import { useSelector } from 'react-redux'
-
+import { useGetRankByIdQuery } from '@redux/services/rankApi'
+import { useEffect, useState } from 'react'
 const getWord = (num) => {
 	if (num % 10 === 1 && num % 100 !== 11) {
 		return 'фантик'
@@ -16,7 +17,6 @@ const getWord = (num) => {
 		return 'фантиков'
 	}
 }
-
 
 // avatarPath: null
 // balance: 0
@@ -34,22 +34,25 @@ const getWord = (num) => {
 // themeId: null
 
 export const UserCard = ({ ShowLvl = true, ShowBalance = true }) => {
-
 	const user = useSelector((state) => state.user.user)
+
+	const { data: rank } = useGetRankByIdQuery(user.rankId)
+
 	
+
 	return (
 		<Link to="/profile">
 			<div className={styles.UserCardWrapper}>
 				<div className={styles.UserInfoWrapper}>
 					<img
 						className={styles.UserCardImage}
-						src={user?.avatarPath ? user?.avatarPath : '../img/User1Avatar.png'}
+						src={user.avatarPath ? user.avatarPath : '../img/User1Avatar.png'}
 					/>
 					<img className={styles.UserFrameImage} src="../img/frame1.png" />
 
 					<div className={styles.UserCardInfo}>
-						<p className={styles.UserName}>{user.displayName}</p>
-						<p className={styles.UserRole}>{user.displayName}</p>
+						<p className={styles.UserName}>{user?.displayName}</p>
+						<p className={styles.UserRole}>{rank?.name}</p>
 					</div>
 				</div>
 				{/* ProgressBar */}
