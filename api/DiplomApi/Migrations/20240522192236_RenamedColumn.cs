7bdbc0cd40f54af538ab152f8f3822d992058ab1
xarlein <alexvolkovdd@gmail.com>
2024-05-27 01:49:59 +0400

    feat(upload): uploading files added

---
+++ api/DiplomApi/Migrations/20240522192236_RenamedColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiplomApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_admin",
+                table: "users",
+                newName: "IsAdmin");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsAdmin",
+                table: "users",
+                newName: "is_admin");
+        }
+    }
+}
