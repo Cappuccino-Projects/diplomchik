d159ffb5decb5f66b63fd8cfbe33aec1bee0cb5d
Ovol <ovol@d3s.ru>
2024-05-30 02:32:55 +0400

    refactor(api): support for composite primary key

--- api/DiplomApi/Repositories/UserProductRepository.cs
+++ api/DiplomApi/Repositories/UserProductRepository.cs
@@ -3,31 +3,12 @@ using DiplomApi.Models.Entities;
 
 namespace DiplomApi.Repositories;
 
-public class UserProductRepository(DbContext context, DbSet<UserProduct> dbSet) : BaseRepository<UserProduct>(context, dbSet)
+public class UserProductRepository(DbContext context, DbSet<UserProduct> dbSet) : BaseCompositeKeyRepository<UserProduct>(context, dbSet)
 {
     public override void Insert(UserProduct entity)
     {
         var product = _context.Set<Product>().Find(entity.ProductId) ?? throw new ArgumentException("Mission does not exist.");
         var user = _context.Set<User>().Find(entity.UserId) ?? throw new ArgumentException("User does not exist.");
         base.Insert(entity);
-    }
-
-    public override UserProduct GetById(int id) => throw new NotSupportedException();
-
-    public UserProduct GetById(int userId, int productId)
-    {
-        return _dbSet.Find(userId, productId);
-    }
-
-    public override void Delete(int id) => throw new NotSupportedException();
-
-    public virtual void Delete(int userId, int productId)
-    {
-        var entity = _dbSet.Find(userId, productId);
-        if (entity == null)
-            return;
-
-        _dbSet.Remove(entity);
-        _context.SaveChanges();
     }
 }
