b2f0778b655a9a81774be2ef4cd964d90df532e7
Ovol <ovol@d3s.ru>
2024-06-13 04:31:22 +0400

    feat(client): add place icon based on place type

--- client/src/Components/PlaceIcon/index.jsx
+++ client/src/Components/PlaceIcon/index.jsx
@@ -1,11 +1,12 @@
 import styles from "./styles.module.css";
+import { getIconPath } from "../../shared/api/getIconPath";
 
 export const PlaceIcon = (props) => {
   return (
     <div className={styles.PlaceIconWrapper}>
       <img
         className={styles.PlaceIconImage}
-        src={"../img/shop.png"}
+        src={getIconPath(props.placeIcon)}
       />
       <p className={styles.PlaceIconText}>{props.placeName}</p>
     </div>
